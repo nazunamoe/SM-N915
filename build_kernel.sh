805060af0f2194e8493d12a86225e780f70d2e28
nazunamoe <jpg3927@gmail.com>
2015-07-05 22:15:51 +0900

    UBERTC 6.0 Toolchain

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
+export CROSS_COMPILE=/home/nazunamoe/note/arm-eabi-6.0/bin/arm-eabi-
 
-make tbeltektt_defconfig
+make SkyHigh_SM-N915S_defconfig
 make
