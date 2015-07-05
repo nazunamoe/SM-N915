0e7e28349fa887c9f87ea8c85e2f0d036f78b32e
nazunamoe <jpg3927@gmail.com>
2015-07-05 22:54:00 +0900

    UBERTC 6.0 > 4.9

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=/home/nazunamoe/note/arm-eabi-6.0/bin/arm-eabi-
+export CROSS_COMPILE=/home/nazunamoe/UBERTC/arm-eabi-4.9/bin/arm-eabi-
 
 make SkyHigh_SM-N915S_defconfig
 make
